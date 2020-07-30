71d201bd9d54b35c613ca13fe954df17ee8bd69f
Shlomi Fish <shlomif@shlomifish.org>
2020-07-30 16:25:23 +0300

    add license

--- install.pl
+++ install.pl
@@ -1,11 +1,25 @@
 #! /usr/bin/env perl
+
+# Copyright (c) 2020 Shlomi Fish ( https://www.shlomifish.org/ )
+# Author: Shlomi Fish ( https://www.shlomifish.org/ )
 #
-# Short description for install.pl
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
 #
-# Author Shlomi Fish <shlomif@cpan.org>
-# Version 0.0.1
-# Copyright (C) 2020 Shlomi Fish <shlomif@cpan.org>
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 use strict;
 use warnings;
 use 5.014;
@@ -13,12 +27,12 @@ use autodie;
 
 use Path::Tiny qw/ path tempdir tempfile cwd /;
 
-my $HOME = $ENV{HOME};
+my $HOME      = $ENV{HOME};
 my $fonts_dir = path($HOME)->child(".fonts");
 $fonts_dir->mkpath();
-foreach my $font (cwd()->children(qr/\.ttf\z/))
+foreach my $font ( cwd()->children(qr/\.ttf\z/) )
 {
     say $font;
-    $font->copy($fonts_dir->child($font->basename()));
+    $font->copy( $fonts_dir->child( $font->basename() ) );
 }
 
